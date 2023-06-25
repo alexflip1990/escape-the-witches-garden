f3ccad1c85cc9d020f07acd7ac77878d2cd9ae9c
alexflip1990 <>
2023-06-25 17:50:26 +0000

    Add introduction section

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" Import libraries for game """
+import sys
+from termcolor import colored, cprint
+import random
+import time
+
+def user_name_intro():
+    """
+     The introduction section, this section explains what the rules for
+     the game are and asks the player to input their name whilst running
+     a while loop to ensure players name is alphabetical.
+    """
+    print("Welcome to Escape the Witche's garden\n")
+    print("The rules are simple\n")
+    print("It's your job to help Jimmy make the right decisions\n")
+    print("And escape the Witche's garden\n")
+    print("Please enter your name\n")
+    username = ""
+    while True:
+        username = input("")
+        if not username.isalpha():
+            print("Sorry numbers can't be accepted")
+            continue
+        else:
+            print("Welcome " + username)
+            print("Let's help Jimmy escape the Witches garden!")
+            
+
+
